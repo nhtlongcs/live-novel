14e48badd5b3cbf46601316adb58f2bab86cda9e
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-05-26 19:12:38 +0000

    init

---
+++ run.sh
@@ -0,0 +1,5 @@
+python  diffusion.py -s 800 480 -i 250 -cuts 4 \
+        -p "a shining lighthouse on the shore of a tropical island in a raging storm | text:-0.99 | watermark:-0.99 | logo:-0.99 | watercolor" \
+        -dvitb32 yes -dvitb16 yes -dvitl14 yes -drn101 no -drn50 yes \
+        -drn50x4 no -drn50x16 no -drn50x64 no -sd 2586166778 \
+        -o output/2022-05-26-example-prompts/a-shining-lighthouse-on-the-shore-of-a-tropical-island-in-a-raging-storm-watercolor.png
