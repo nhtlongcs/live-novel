0131d8e59cdf4a6331fa8f4b69a039e9d912b36b
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-08-27 08:52:59 +0000

    refactor sth

--- create_fn.py
+++ create_fn.py
@@ -1,123 +1,48 @@
 # python scripts/txt2img.py --prompt "a photograph of an astronaut riding a horse" --plms 
 
-import argparse, os, sys, glob
-from asyncio.log import logger
-# sys.path.insert(0, "./stable-diffusion")
-import cv2
-import torch
-import numpy as np
-from omegaconf import OmegaConf
-from PIL import Image
-from tqdm import tqdm, trange
-from imwatermark import WatermarkEncoder
-from itertools import islice
-from einops import rearrange
-from torchvision.utils import make_grid
-import time
-from pytorch_lightning import seed_everything
-from torch import autocast
-from contextlib import contextmanager, nullcontext
-
-from stable_diffusion.ldm.util import instantiate_from_config
-from stable_diffusion.ldm.models.diffusion.ddim import DDIMSampler
-from stable_diffusion.ldm.models.diffusion.plms import PLMSSampler
-
-from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
-from transformers import AutoFeatureExtractor
-
-import gc 
-# load safety model
-safety_model_id = "stable_diffusion/models/stable-diffusion-safety-checker"
-safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
-safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
-
+import argparse
 # ======== create 
 import copy
+import glob
 import multiprocessing
 import os
+import sys
+import time
 import warnings
+from asyncio.log import logger
+from contextlib import contextmanager, nullcontext
+from itertools import islice
 from types import SimpleNamespace
-from typing import overload, List, Optional, Dict, Any, Union, TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union, overload
 
+import cv2
 import numpy as np
+import torch
+from diffusers.pipelines.stable_diffusion.safety_checker import \
+    StableDiffusionSafetyChecker
+from einops import rearrange
+from imwatermark import WatermarkEncoder
+from omegaconf import OmegaConf
+from PIL import Image
+from pytorch_lightning import seed_everything
+from torch import autocast
+from torchvision.utils import make_grid
+from tqdm import tqdm, trange
+from transformers import AutoFeatureExtractor
+
+from stable_diffusion.ldm.models.diffusion.ddim import DDIMSampler
+from stable_diffusion.ldm.models.diffusion.plms import PLMSSampler
+from stable_diffusion.ldm.util import instantiate_from_config
+from utils import (check_safety, chunk, free_memory, load_model_from_config,
+                   numpy_to_pil, put_watermark)
 
 if TYPE_CHECKING:
     import threading
     import asyncio
 
-from docarray import DocumentArray, Document
+from docarray import Document, DocumentArray
 
 _models_cache = {}
-
-def chunk(it, size):
-    it = iter(it)
-    return iter(lambda: tuple(islice(it, size)), ())
-
-
-def numpy_to_pil(images):
-    """
-    Convert a numpy image or a batch of images to a PIL image.
-    """
-    if images.ndim == 3:
-        images = images[None, ...]
-    images = (images * 255).round().astype("uint8")
-    pil_images = [Image.fromarray(image) for image in images]
-
-    return pil_images
-
-
-def load_model_from_config(config, ckpt, verbose=False):
-    print(f"Loading model from {ckpt}")
-    pl_sd = torch.load(ckpt, map_location="cpu")
-    if "global_step" in pl_sd:
-        print(f"Global Step: {pl_sd['global_step']}")
-    sd = pl_sd["state_dict"]
-    model = instantiate_from_config(config.model)
-    m, u = model.load_state_dict(sd, strict=False)
-    if len(m) > 0 and verbose:
-        print("missing keys:")
-        print(m)
-    if len(u) > 0 and verbose:
-        print("unexpected keys:")
-        print(u)
-
-    model.cuda()
-    model.eval()
-    return model
-
-
-def put_watermark(img, wm_encoder=None):
-    if wm_encoder is not None:
-        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        img = wm_encoder.encode(img, 'dwtDct')
-        img = Image.fromarray(img[:, :, ::-1])
-    return img
-
-
-def load_replacement(x):
-    try:
-        hwc = x.shape
-        y = Image.open("assets/rick.jpeg").convert("RGB").resize((hwc[1], hwc[0]))
-        y = (np.array(y)/255.0).astype(x.dtype)
-        assert y.shape == x.shape
-        return y
-    except Exception:
-        return x
-
-
-def check_safety(x_image):
-    safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
-    x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
-    assert x_checked_image.shape[0] == len(has_nsfw_concept)
-    for i in range(len(has_nsfw_concept)):
-        if has_nsfw_concept[i]:
-            x_checked_image[i] = load_replacement(x_checked_image[i])
-    return x_checked_image, has_nsfw_concept
-
-
-def free_memory():
-    gc.collect()
-    torch.cuda.empty_cache()
 
 _flag = False
 def create(
