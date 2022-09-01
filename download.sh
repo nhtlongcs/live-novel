080a2f212ddbc7b3a770bd1a7cdca742db0744e9
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2022-09-01 13:27:21 +0000

    add model download script

---
+++ download.sh
@@ -0,0 +1,2 @@
+gdown "https://drive.google.com/uc?id=1dNPc_UjdjSsimRxziMte2NhHpymIH8xx" -O weight.zip
+unzip -y weight.zip -d stable_diffusion/models/
--- stable_diffusion/models/ldm/huggingface/README.md
+++
@@ -1,1 +0,0 @@
-../../blobs/74be0f47467c2f54e410c882b6c68a38f06ef2ff
